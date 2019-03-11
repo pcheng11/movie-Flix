5c5d81a9c4157c838236031df58e7d358d70afb3
STSMAC1 <STSMAC1@stsmac1s-imac.ur.rochester.edu>
2019-03-11 14:00:54 -0400

    Updates

--- precache-manifest.afa3ee170901c966e0c7ae5a084c66c3.js
+++ precache-manifest.afa3ee170901c966e0c7ae5a084c66c3.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/movie-Flix/static/js/runtime~main.7540e249.js"
   },
   {
-    "revision": "40d4bfc9b45361f59c63",
-    "url": "/movie-Flix/static/js/main.537c5044.chunk.js"
+    "revision": "8a29367ad1260eee1a79",
+    "url": "/movie-Flix/static/js/main.31e928ec.chunk.js"
   },
   {
     "revision": "cd5bca88f6cc883faab8",
     "url": "/movie-Flix/static/js/2.d83237a7.chunk.js"
   },
   {
-    "revision": "40d4bfc9b45361f59c63",
+    "revision": "8a29367ad1260eee1a79",
     "url": "/movie-Flix/static/css/main.ba833028.chunk.css"
   },
   {
-    "revision": "e53c482456e8b9a79ed7737cecbc4920",
+    "revision": "96172b0ba0d868a9158cbe68ea974f04",
     "url": "/movie-Flix/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-Flix/precache-manifest.c05a11f26695e675cdb8659b16bffbe5.js"
+  "/movie-Flix/precache-manifest.afa3ee170901c966e0c7ae5a084c66c3.js"
 );
 
 workbox.clientsClaim();
