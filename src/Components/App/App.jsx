dcf38f79e06c6408080716dc89d9d940bd0e601e
Eris.Song <Eris.Song@appledeMacBook-Pro-4.local>
2019-03-11 11:57:13 -0400

    k

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HistoryRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from '../Home/Home.jsx'
 import Detail from '../Detail/Detail.jsx'
 import GalleryView from '../Gallery/GalleryView.jsx'
@@ -8,13 +8,13 @@ let hashHistory = Router.hashHistory;
 class App extends React.Component {
     render() {
         return (
-            <HistoryRouter>
+            <BrowserRouter>
                 <Switch>
                     <Route path={process.env.PUBLIC_URL + '/'} component={Home} />
                     <Route path={process.env.PUBLIC_URL + '/detail'} component={Detail} />
                     <Route path={process.env.PUBLIC_URL + '/gallery'} component={GalleryView} />
                 </Switch>
-            </HistoryRouter>
+            </BrowserRouter>
         );
     }
 }
