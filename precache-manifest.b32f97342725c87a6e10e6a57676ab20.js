01f686acf97e1ca53f3b2a1243e6d29eb93a668c
STSMAC1 <STSMAC1@stsmac1s-imac.ur.rochester.edu>
2019-03-11 14:20:21 -0400

    Updates

---
+++ precache-manifest.b32f97342725c87a6e10e6a57676ab20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "452a498b43420a72f158",
+    "url": "/movie-Flix/static/js/runtime~main.7540e249.js"
+  },
+  {
+    "revision": "395d8d84836a81e44167",
+    "url": "/movie-Flix/static/js/main.d487d9ae.chunk.js"
+  },
+  {
+    "revision": "9b54e503415faa4e67e8",
+    "url": "/movie-Flix/static/js/2.501c6fc3.chunk.js"
+  },
+  {
+    "revision": "395d8d84836a81e44167",
+    "url": "/movie-Flix/static/css/main.ba833028.chunk.css"
+  },
+  {
+    "revision": "59c338caf7c8bcf2b3559f9bc67c8304",
+    "url": "/movie-Flix/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-Flix/precache-manifest.9b567a80b712fa9a67e0a2134f70340a.js"
+  "/movie-Flix/precache-manifest.b32f97342725c87a6e10e6a57676ab20.js"
 );
 
 workbox.clientsClaim();
