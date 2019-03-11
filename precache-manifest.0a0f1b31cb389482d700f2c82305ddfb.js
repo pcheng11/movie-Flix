57dc9fa6b053b934af4b60b209fc3406effa8e8f
Eris.Song <Eris.Song@appledeMacBook-Pro-4.local>
2019-03-11 11:43:34 -0400

    Updates

--- precache-manifest.0a0f1b31cb389482d700f2c82305ddfb.js
+++ precache-manifest.0a0f1b31cb389482d700f2c82305ddfb.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/movie-Flix/static/js/runtime~main.7540e249.js"
   },
   {
-    "revision": "7e67506d7481c2222c78",
-    "url": "/movie-Flix/static/js/main.bd83b350.chunk.js"
+    "revision": "9364f2ab338e5ff18944",
+    "url": "/movie-Flix/static/js/main.4083c34f.chunk.js"
   },
   {
     "revision": "8f55838987897600f2c7",
     "url": "/movie-Flix/static/js/2.3ab38b3f.chunk.js"
   },
   {
-    "revision": "7e67506d7481c2222c78",
+    "revision": "9364f2ab338e5ff18944",
     "url": "/movie-Flix/static/css/main.f9b3c37c.chunk.css"
   },
   {
-    "revision": "00e443d4e50e1d426eed1eceee130f3c",
+    "revision": "66876bd217b5be15599559bef11844b0",
     "url": "/movie-Flix/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-Flix/precache-manifest.abdd843d55c45258791f4b9c31d8513f.js"
+  "/movie-Flix/precache-manifest.0a0f1b31cb389482d700f2c82305ddfb.js"
 );
 
 workbox.clientsClaim();
