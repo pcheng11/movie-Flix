558dde1a9c2b8e96b4a44822ac77ae9de5fae581
STSMAC1 <STSMAC1@stsmac1s-imac.ur.rochester.edu>
2019-03-11 14:02:14 -0400

    Updates

--- precache-manifest.5075ee6908a8d8cde8c93c750ca8823f.js
+++ precache-manifest.5075ee6908a8d8cde8c93c750ca8823f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/movie-Flix/static/js/runtime~main.7540e249.js"
   },
   {
-    "revision": "8a29367ad1260eee1a79",
-    "url": "/movie-Flix/static/js/main.31e928ec.chunk.js"
+    "revision": "8bee1f6cdabf76012dcf",
+    "url": "/movie-Flix/static/js/main.bb6f384f.chunk.js"
   },
   {
     "revision": "cd5bca88f6cc883faab8",
     "url": "/movie-Flix/static/js/2.d83237a7.chunk.js"
   },
   {
-    "revision": "8a29367ad1260eee1a79",
+    "revision": "8bee1f6cdabf76012dcf",
     "url": "/movie-Flix/static/css/main.ba833028.chunk.css"
   },
   {
-    "revision": "96172b0ba0d868a9158cbe68ea974f04",
+    "revision": "d1fad4b06199187b88a792cdc3f38eba",
     "url": "/movie-Flix/index.html"
   }
 ];
