88e2456d0b922d192f71c4262b4c6598c5a3eee3
STSMAC1 <STSMAC1@stsmac1s-imac.ur.rochester.edu>
2019-03-11 14:42:31 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-Flix/precache-manifest.1badb87cb9273782f0f65e48ee03797d.js"
+  "/movie-Flix/precache-manifest.d1bc1a3cf0cc0288a717eac6d23d6f36.js"
 );
 
 workbox.clientsClaim();
