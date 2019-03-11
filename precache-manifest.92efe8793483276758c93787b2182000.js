1dd6ad35bd5a371b047647c44be8dbb6855c9c33
Eris.Song <Eris.Song@appledeMacBook-Pro-4.local>
2019-03-11 12:35:12 -0400

    Updates

---
+++ precache-manifest.92efe8793483276758c93787b2182000.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "452a498b43420a72f158",
+    "url": "/movie-Flix/static/js/runtime~main.7540e249.js"
+  },
+  {
+    "revision": "fe0c7d08b5dd7dd7c76c",
+    "url": "/movie-Flix/static/js/main.7d12ee6d.chunk.js"
+  },
+  {
+    "revision": "cd5bca88f6cc883faab8",
+    "url": "/movie-Flix/static/js/2.d83237a7.chunk.js"
+  },
+  {
+    "revision": "fe0c7d08b5dd7dd7c76c",
+    "url": "/movie-Flix/static/css/main.f9b3c37c.chunk.css"
+  },
+  {
+    "revision": "8e5aa505ccc1951d48c546c76d298a11",
+    "url": "/movie-Flix/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-Flix/precache-manifest.c0d98295aee60576b1c446091e2e2968.js"
+  "/movie-Flix/precache-manifest.92efe8793483276758c93787b2182000.js"
 );
 
 workbox.clientsClaim();
